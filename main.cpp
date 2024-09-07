8011d7413f0975a037f3111be8ceeac205f0322a
tak-sillydev <takumi.tsuchiya718@gmail.com>
2024-09-08 02:36:21 +0900

    use getaddrinfo

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
-#include <stdio.h>
+/*
+	試作品：IPv4 only 2024-09-07
+*/
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include <iostream>
 
-// union のメモリ配置の仕様の関係で、
-// partsに値を入れるとaddressから正しいエンディアンで取り出せる
-typedef union {
-	unsigned int	address;
-	struct {
-		unsigned char	a0, a1, a2, a3;
-	} parts;
-} IPV4_ADDR;
+#include "json.hpp"
 
-#define	PORT_TX	1111	// 送り側 srt-live-transmit は相手側コンピュータのこのポートに送る
-#define	PORT_RX	2222	// 受け側 srt-live-transmit は自分側コンピュータのこのポートからデータを取る
-#define	PATHWAY_NUM	2
+using namespace std;
 
 int main(void) {
 	fd_set	fds, rdfds;
 	int		nfds;
 
-	const char* devlist[PATHWAY_NUM] = {"enp0s25", "wlp2s0"};
-	const int	npathway = PATHWAY_NUM;
+	ifstream	ifs("./config.json");
 
-	sockaddr_in	addr_tx_in, addr_tx_out;
-	sockaddr_in	*addr_rx_in, addr_rx_out;
-	IPV4_ADDR	v4addr;
+	if (ifs.fail()) {
+		cout << "FAIL: open config.json" << endl;
+		exit(1);
+	}
+	auto	config = nlohmann::json::parse(ifs);
+	auto	device_list = config["device_list"];
+	int		ndevices = device_list.size();
+
+	// tx_out (CAM側,接続元) <--> [ tx_in - rx_in ] <--> rx_out（モニタ側,接続先）
+	sockaddr_in6	addr_tx_in, addr_tx_out;
+	sockaddr_in		*addr_rx_in, addr_rx_out;
 
 	int			sock_tx, *sock_rx;	// それぞれTX側、RX側と通信するためのソケット
 	int			rxmax = 0, counter = 0;
@@ -39,36 +43,58 @@ int main(void) {
 	ssize_t			szrecv, selret;
 
 	char	txbuf[2048], rxbuf[2048];
+	char	addrbuf[64];
 
 	// TX側ソケット準備
-	sock_tx = socket(AF_INET, SOCK_DGRAM, 0);
+	sock_tx = socket(AF_INET6, SOCK_DGRAM, 0);
 
 	if (sock_tx == -1) { printf("FATAL: create TX socket\n"); return 0; }
 
 	setsockopt(sock_tx, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
 	// リレーTX側　待受ポート指定
-	addr_tx_in.sin_family		= AF_INET;
-	addr_tx_in.sin_addr.s_addr	= INADDR_ANY;
-	addr_tx_in.sin_port			= htons(PORT_TX);
+	addr_tx_in.sin6_family		= AF_INET6;
+	addr_tx_in.sin6_addr		= IN6ADDR_ANY_INIT;
+	addr_tx_in.sin6_port		= htons(config["receive_at"]["port"]);
 	bind(sock_tx, (sockaddr *)&addr_tx_in, sizeof(addr_tx_in));
 
 	FD_ZERO(&rdfds);
 	FD_SET(sock_tx, &rdfds);
 
 	// RX側ソケット準備
-	sock_rx = new int[npathway];
-	addr_rx_in = new sockaddr_in[npathway];
+	sock_rx = new int[ndevices];
+	addr_rx_in = new sockaddr_in[ndevices];
 
-	if (sock_rx == nullptr) { printf("FATAL: allocate rx socket\n"); return 0; }
+	if (sock_rx == nullptr) {
+		cout << "FATAL: allocate RX socket" << endl;
+		exit(1);
+	}
+	string		sendto_addr = config["send_to"]["address"];
+	uint16_t	sendto_port = config["send_to"]["port"];
+	char		port_str[16];
+	addrinfo	hints, *res;
 
-	for (int i = 0; i < npathway; i++) {
-		sock_rx[i] = socket(AF_INET, SOCK_DGRAM, 0);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family		= AF_INET;
+	hints.ai_socktype	= SOCK_DGRAM;
 
-		if (sock_rx[i] == -1) { printf("FATAL: create RX socket\n"); return 0; }
+	sprintf(port_str, "%hu", sendto_port);
+	if (getaddrinfo(sendto_addr.c_str(), port_str, &hints, &res) < 0) {
+		cout << "FATAL: get address info" << endl;
+		exit(1);
+	}
+	// リレーRX側　接続先ポート指定
+	addr_rx_out = *reinterpret_cast<sockaddr_in*>(res->ai_addr);
+
+	for (int i = 0; i < ndevices; i++) {
+		const char	*device = device_list[i].get<string>().c_str();
+
+		sock_rx[i] = socket(res->ai_family, res->ai_socktype, 0);
+
+		if (sock_rx[i] == -1) { cout << "FATAL: create RX socket" << endl; return 0; }
 
 		setsockopt(sock_rx[i], SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-		setsockopt(sock_rx[i], SOL_SOCKET, SO_BINDTODEVICE, devlist[i], strlen(devlist[i]));
+		setsockopt(sock_rx[i], SOL_SOCKET, SO_BINDTODEVICE, device, strlen(device));
 
 		// リレーRX側　待受ポート指定
 		addr_rx_in[i].sin_family		= AF_INET;
@@ -77,18 +103,10 @@ int main(void) {
 		bind(sock_rx[i], (sockaddr *)&addr_rx_in[i], sizeof(sockaddr_in));
 		getsockname(sock_rx[i], (sockaddr *)&addr_rx_in[i], &szaddr);		// bind() によって使用ポートが割り当てられたので書き戻し
 
-		// リレーRX側　接続先ポート指定
-		v4addr.parts.a0 = 192;
-		v4addr.parts.a1 = 168;
-		v4addr.parts.a2 =   0;
-		v4addr.parts.a3 =  11;
-		addr_rx_out.sin_family		= AF_INET;
-		addr_rx_out.sin_addr.s_addr	= v4addr.address;
-		addr_rx_out.sin_port		= htons(2222);
-
 		FD_SET(sock_rx[i], &rdfds);
 		rxmax = (rxmax > sock_rx[i]) ? rxmax : sock_rx[i];
 	}
+	freeaddrinfo(res);
 	nfds = (sock_tx > rxmax) ? sock_tx : rxmax;
 
 	memset(txbuf, 0, 2048);
@@ -118,50 +136,42 @@ int main(void) {
 			if (szrecv == -1) {
 				//printerr()
 			}
-			v4addr.address = addr_tx_out.sin_addr.s_addr;
-			printf(
-				"FROM TX %d.%d.%d.%d:%d (%ldbytes) --> [RELAY IN %d > ",
-				v4addr.parts.a0, v4addr.parts.a1, v4addr.parts.a2, v4addr.parts.a3,
-				htons(addr_tx_out.sin_port), szrecv, htons(addr_tx_in.sin_port)
-			);
+			inet_ntop(addr_tx_out.sin6_family, &addr_tx_out.sin6_addr, addrbuf, sizeof(addrbuf));
+			cout << "FROM TX " << addrbuf << ":" << htons(addr_tx_out.sin6_port) <<
+				"(" << szrecv << "bytes) " <<
+				"--> [RELAY IN " << htons(addr_tx_in.sin6_port) << " > ";
 
 			// 受信したデータをRX側へ流す
 			sendto(sock_rx[counter], txbuf, szrecv, 0, (sockaddr *)&addr_rx_out, sizeof(addr_rx_out));
 
-			v4addr.address = addr_rx_out.sin_addr.s_addr;
-			printf(
-				"OUT %d] --> TO RX %d.%d.%d.%d:%d\n",
-				htons(addr_rx_in[counter].sin_port),
-				v4addr.parts.a0, v4addr.parts.a1, v4addr.parts.a2, v4addr.parts.a3, htons(addr_rx_out.sin_port)
-			);
-			if (++counter == npathway) { counter = 0; }
+			inet_ntop(addr_rx_out.sin_family, &addr_rx_out.sin_addr, addrbuf, sizeof(addrbuf));
+			cout << "OUT " << htons(addr_rx_in[counter].sin_port) << "] --> TO RX " <<
+				addrbuf << ":" << htons(addr_rx_out.sin_port) << endl;
+			
+			if (++counter == ndevices) { counter = 0; }
 		}
-		for (int i = 0; i < npathway; i++) {
+		for (int i = 0; i < ndevices; i++) {
 			if (FD_ISSET(sock_rx[i], &fds)) {
 				// RX側からデータ受信
 				// リレーRX側　接続先ポート取得（ユーザ指定値なのでやらなくてもいい）
 				szrecv = recvfrom(sock_rx[i], rxbuf, 2048, MSG_WAITALL, (sockaddr *)&addr_rx_out, &szaddr);
 
-				v4addr.address = addr_rx_out.sin_addr.s_addr;
-				printf(
-					"FROM RX %d.%d.%d.%d:%d (%ldbytes) --> [RELAY IN %d > ",
-					v4addr.parts.a0, v4addr.parts.a1, v4addr.parts.a2, v4addr.parts.a3,
-					htons(addr_rx_out.sin_port), szrecv, htons(addr_rx_in[i].sin_port)
-				);
+				inet_ntop(addr_rx_out.sin_family, &addr_rx_out.sin_addr, addrbuf, sizeof(addrbuf));
+				cout << "FROM RX " << addrbuf << ":" << htons(addr_rx_out.sin_port) <<
+					"(" << szrecv << "bytes) " <<
+					"--> [RELAY IN " << htons(addr_rx_in[i].sin_port) << "> ";
 
 				// 受信したデータをTX側へ流す
 				sendto(sock_tx, rxbuf, szrecv, 0, (sockaddr *)&addr_tx_out, sizeof(addr_tx_out));
 
-				v4addr.address = addr_tx_out.sin_addr.s_addr;
-				printf(
-					"OUT %d] --> TO TX %d.%d.%d.%d:%d\n",
-					htons(addr_tx_in.sin_port),
-					v4addr.parts.a0, v4addr.parts.a1, v4addr.parts.a2, v4addr.parts.a3, htons(addr_tx_out.sin_port)
-				);
+				inet_ntop(addr_tx_out.sin6_family, &addr_tx_out.sin6_addr, addrbuf, sizeof(addrbuf));
+				cout << "OUT " << htons(addr_tx_in.sin6_port) << "] --> TO TX " <<
+					addrbuf << ":" << htons(addr_tx_out.sin6_port) << endl;
 			}
 		}
 	}
 	close(sock_tx);
-	for (int i = 0; i < npathway; i++) { close(sock_rx[i]); }
+	for (int i = 0; i < ndevices; i++) { close(sock_rx[i]); }
+
 	return 0;
 }
